f5ba5c2ed636edc63bc7421f5dacaa02ae757bcd
Dylan <53993453+dxs5670@users.noreply.github.com>
2020-03-31 14:34:42 -0400

    An update for hello.java to do something cool

---
+++ hello.java
@@ -0,0 +1,24 @@
+public class hello{
+
+  
+
+
+
+  public int doSomethingCool(){
+    let a = 25;
+    let b = 40;
+    let c = 20;
+    let d = 4;
+    for (int i = 20; i <= 25; i++) {
+      a += b * c / 4;
+    }
+    
+    return a;
+  }
+
+  public static void main(String[] args){
+    System.out.println(doSomethingCool());
+  
+  }
+  
+}
